773a25c07b2c1236760860ea6bd22293c07497e1
seanrogan <srogan88@gmail.com>
2022-04-22 20:28:36 -0400

    initial commit - setting up maven project and adding dependencies

---
+++ src/main/java/com/seanrogandev/alltrax/App.java
@@ -0,0 +1,31 @@
+package com.seanrogandev.alltrax;
+
+/**
+ * ALLTRAX MUSIC APP
+ * an app to get ALL TRACKS
+ * from an artist in chronological order!
+ *
+ * Have you ever stumbled upon a song
+ * by some unknown artist while listening to spotify,
+ * and you think "dang, this artist is good...
+ * or, at least, this SONG is really good!
+ * i wonder if this artist is worth checking out?"
+ * So you do. you click on the artist
+ * and spotify is kind enough to show you their top 5 songs!
+ * well what if you want to dig a little deeper?
+ * thats where it becomes difficult...
+ * you have to search their discography and manually check
+ * each album and listen through. Many artists have mostly
+ * singles which makes it even more tedious.
+ *
+ * This app is to solve that problem.
+ * Enter an artists name, and get back a playlist of every one
+ * of the songs that artist has put out, in chronological order.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
